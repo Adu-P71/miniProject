4bf5017a4a734b5d99169f4ad190f4132c2777b0
adupoku <davidswimmer71@gmail.com>
2021-07-09 16:50:40 +0000

    solved path errors

--- Routes/index.js
+++ Routes/index.js
@@ -1,7 +1,8 @@
 const { Router } = require("express")
 const router = Router()
-
-const { getPage } = require("../Controllers/indexController")
+const path = require("path")
+const indexPath = path.join(__dirname, "Controllers/indexController.js")
+const { getPage } = require(indexPath)
 
 router.get("/", getPage)
 

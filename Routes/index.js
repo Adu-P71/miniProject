18f0f4412168849434b0ca8fb67111cba467f24c
adupoku <davidswimmer71@gmail.com>
2021-07-10 08:37:23 +0000

    still on routing : no major changes

--- Routes/index.js
+++ Routes/index.js
@@ -2,7 +2,7 @@ const { Router } = require("express")
 const router = Router()
 const path = require("path")
 // const indexPath = "./Controllers/indexController.js"
-const { getPage } = require(indexPath)
+// const { getPage } = require(indexPath)
 
 router.get("/", (req, res) => {
   res.send("app")

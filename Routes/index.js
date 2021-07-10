edc0eec61be5698804c0a8fda7d7063714bbe7fd
adupoku <davidswimmer71@gmail.com>
2021-07-10 08:13:33 +0000

    still on routing

--- Routes/index.js
+++ Routes/index.js
@@ -1,7 +1,7 @@
 const { Router } = require("express")
 const router = Router()
 const path = require("path")
-const indexPath = path.join(__dirname, "Controllers/indexController.js")
+const indexPath = "/Controllers/indexController.js"
 const { getPage } = require(indexPath)
 
 router.get("/", getPage)

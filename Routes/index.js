932a42edc834fe4b43a4f55b1fae1fbdecf42fa9
adupoku <davidswimmer71@gmail.com>
2021-07-10 09:07:37 +0000

    used path.normalize for the index route:no major changes

--- Routes/index.js
+++ Routes/index.js
@@ -1,7 +1,8 @@
 const { Router } = require("express")
 const router = Router()
 const path = require("path")
-const indexPath = path.normalize("../Controllers/indexController.js")
+const indexPath = path.normalize("indexController.js")
+console.log(indexPath)
 const { getPage } = require(indexPath)
 
 router.get("/", getPage)

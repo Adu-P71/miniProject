1c5982de1d4cc5c9fd9aed3ba545c0bbf8b76a0e
adupoku <davidswimmer71@gmail.com>
2021-07-10 09:33:32 +0000

    put dots in front of path of static file scripts

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(PRODUCTS)
 app.use(expressLayouts)
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
-app.use(express.static("public"))
+app.use(express.static(__dirname + "/public"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
